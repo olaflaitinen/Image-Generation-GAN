d25990323eda6ca77ff05e459730e7ec2c9359da
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:27:13 +0200

    Create test_evaluate.py

---
+++ tests/test_evaluate.py
@@ -0,0 +1,10 @@
+import unittest
+from src.evaluate import evaluate_gan
+
+class TestEvaluate(unittest.TestCase):
+    def test_evaluate_gan(self):
+        # Test GAN değerlendirme fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

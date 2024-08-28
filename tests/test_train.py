23d5481f2d9cb6b38561989b87e4c9a6858d1e32
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:26:56 +0200

    Create test_train.py

---
+++ tests/test_train.py
@@ -0,0 +1,10 @@
+import unittest
+from src.train import train_gan
+
+class TestTrain(unittest.TestCase):
+    def test_train_gan(self):
+        # Test GAN eğitme fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

36b361b9d442781ba508d135dffe25ed7351b5b5
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:26:37 +0200

    Create test_model.py

---
+++ tests/test_model.py
@@ -0,0 +1,14 @@
+import unittest
+from src.model import build_generator, build_discriminator
+
+class TestModel(unittest.TestCase):
+    def test_build_generator(self):
+        # Test generator model oluşturma fonksiyonu
+        pass
+
+    def test_build_discriminator(self):
+        # Test discriminator model oluşturma fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
